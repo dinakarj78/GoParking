d0186df21ef665c1c94606600eb4666aa3f25ec0
Dinakar Jonnalagadda <jonnalagaddadinakar4848@gmail.com>
2023-07-26 17:07:25 -0400

    add lot/lots based on location.

--- API/lots.go
+++ API/lots.go
@@ -4,7 +4,7 @@ import (
 	"GoParking/models"
 	"net/http"
 	"github.com/gin-gonic/gin"
-	uuid "github.com/satori/go.uuid"
+	// uuid "github.com/satori/go.uuid"
 )
 
 func GetDetails(c *gin.Context) {
@@ -18,14 +18,15 @@ func Addlots(c *gin.Context) {
 	// c.IndentedJSON(http.StatusCreated, newlot)
 	var i int =0
     for _,lot:=range newlot{
-		newlotmap:=make(map[uuid.UUID]string)
-        newlotmap[lot.Id]=lot.Location
+		// newlotmap:=make(map[uuid.UUID]string)
+        // newlotmap[lot.Id]=lot.Location
 		if models.Parkinglotstruct[i].Location==lot.Location{
 	models.Parkinglotstruct[i].Lots=append(models.Parkinglotstruct[i].Lots, lot)
-	c.IndentedJSON(http.StatusCreated, newlot)
-}
+   }else{
     i++
+   }
 }
+c.IndentedJSON(http.StatusCreated, models.Parkinglotstruct)
 
 	// for _, lots := range models.Parkinglotstruct {
 	// 	if lots.Location == newlot.Location {
