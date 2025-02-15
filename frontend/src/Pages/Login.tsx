c85fa75329da5cf6c5d65b3052102c407899f298
Dinakar Jonnalagadda <53013661+dinakarj78@users.noreply.github.com>
2025-02-14 21:50:14 -0600

    feat: implemented FE code

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from "react"
+import React, { Ref, RefObject, useEffect, useRef, useState } from "react"
 import styles from './styles.module.scss'
 import emailIcon from './../assets/emailIcon.svg'
 import passwordIcon from './../assets/passwordIcon.svg'
 import poster from './../assets/Parking.png'
 import mobileLogo from './../assets/Website_logo.png'
 
-function validateLogin() {
-
+function validateLogin(emailValue:RefObject<HTMLInputElement>,passwordValue:RefObject<HTMLInputElement>) {
+  if(emailValue.current.value == null || passwordValue.current.value==null)
+  {
+    alert('please enter all fields')
+  }
+  if(!emailValue.current.value.includes('@',3) || passwordValue.current.value.length<=4){
+    alert('either password is less than 5 charcaters or incorrect value entered for email')
+  }
 }
 export function Login() {
+
+  const emailValue=useRef<HTMLInputElement>(null);
+  const passwordValue=useRef<HTMLInputElement>(null);
   const [width, setWidth] = useState<number>(window.innerWidth);
 
 function handleWindowSizeChange() {
@@ -34,16 +43,16 @@ const isMobile = width <= 720;
           <h2>Login to access your account</h2>
           <label>
             <img src={emailIcon} />
-            <input type="input" placeholder="Your Email" />
+            <input type="input" placeholder="Your Email" ref={emailValue} required/>
           </label>
         </div>
         <div className="password">
           <label>
             <img src={passwordIcon} />
-            <input type="password" placeholder="Your Password" />
+            <input type="password" placeholder="Your Password" ref={passwordValue}required />
           </label>
         </div>
-        <button type="submit" className={styles.loginButton} onClick={validateLogin}>Log in</button>
+        <button type="submit" className={styles.loginButton} onClick={()=>{validateLogin(emailValue,passwordValue)}}>Log in</button>
       </div>
       <div className={styles.login_Banner}>
         <img src={poster} />
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { Login } from './Pages/Login.tsx';
 import reportWebVitals from './reportWebVitals.ts';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+     <Routes>
+     <Route path="/" element={<Login />} />
+     </Routes>
+    </BrowserRouter>
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
