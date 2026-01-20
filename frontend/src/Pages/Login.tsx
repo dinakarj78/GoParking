386b2e540badcfbac1f154e8d70362564a52a156
dinakarj78 <jonnalagaddadinakar4848@gmail.com>
2026-01-19 22:44:14 -0600

    chore: migrat front-end to react

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,9 +1,9 @@
 import React, { Ref, RefObject, useEffect, useRef, useState } from "react"
 import styles from './styles.module.scss'
-import emailIcon from './../assets/emailIcon.svg'
-import passwordIcon from './../assets/passwordIcon.svg'
-import poster from './../assets/Parking.png'
-import mobileLogo from './../assets/Website_logo.png'
+import emailIcon from './../../public/assets/emailIcon.svg'
+import passwordIcon from './../../public/assets/passwordIcon.svg'
+import poster from './../../public/assets/Parking.png'
+import mobileLogo from './../../public/assets/Website_logo.png'
 
 function validateLogin(emailValue:RefObject<HTMLInputElement>,passwordValue:RefObject<HTMLInputElement>) {
   if(emailValue.current.value == null || passwordValue.current.value==null)
@@ -36,26 +36,26 @@ const isMobile = width <= 720;
     <div className={styles.LoginContainer}>
        {
       isMobile&&
-      <img  className={styles.mobile_Logo}src={mobileLogo}/>
+      <img  className={styles.mobile_Logo}src={mobileLogo.src}/>
     }
       <div className={styles.login_Input_Block}>
         <div className="email">
           <h2>Login to access your account</h2>
           <label>
-            <img src={emailIcon} />
+            <img src={emailIcon.src} />
             <input type="input" placeholder="Your Email" ref={emailValue} required/>
           </label>
         </div>
         <div className="password">
           <label>
-            <img src={passwordIcon} />
+            <img src={passwordIcon.src} />
             <input type="password" placeholder="Your Password" ref={passwordValue}required />
           </label>
         </div>
         <button type="submit" className={styles.loginButton} onClick={()=>{validateLogin(emailValue,passwordValue)}}>Log in</button>
       </div>
       <div className={styles.login_Banner}>
-        <img src={poster} />
+        <img src={poster.src} />
       </div>
     </div>
   )
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
 import { Login } from './Pages/Login.tsx';
 import reportWebVitals from './reportWebVitals.ts';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,11 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
      <Routes>
      <Route path="/" element={<Login />} />
      </Routes>
-    </BrowserRouter>
     </React.StrictMode>
 
 );
